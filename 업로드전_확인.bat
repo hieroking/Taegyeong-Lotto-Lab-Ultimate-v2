442c33da81acec02dd4a3c1afaceb856da885e2f
hieroking <hieroking@naver.com>
2026-07-15 10:06:51 +0900

    Add files via upload

---
+++ 업로드전_확인.bat
@@ -0,0 +1,16 @@
+﻿@echo off
+chcp 65001 >nul
+echo.
+echo 太炅 Lotto Lab Ultimate v5.1 GitHub 업로드 준비
+echo.
+echo 이 폴더 안의 아래 5개 항목을 GitHub 업로드 창으로 드래그하세요.
+echo   Android
+echo   Windows
+echo   .github
+echo   README.md
+echo   업로드순서.txt
+echo.
+echo 중요: .github 폴더가 안 보이면 파일 탐색기 상단
+echo 보기 ^> 표시 ^> 숨긴 항목을 체크하세요.
+echo.
+pause
